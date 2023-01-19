9a8971bd85a3d4d6e9c1419edc2d60afe69c12e5
zeroThousand111 <zeroThousand111@proton.me>
2023-01-19 17:11:50 +0000

    add second else/if block on p40

--- flowmatic_rules.rb
+++ flowmatic_rules.rb
@@ -7,3 +7,18 @@ elsif !flowmatic_on
 else 
   puts "No water!"
 end
+
+# The flow_rate can be reset with the value below to change the outcome of the if/else block below (remove the #)
+# flow_rate = 100
+
+if flow_rate > 50
+  puts "Warning! flow_rate is above 50! It is #{flow_rate}."
+  flow_rate = 50
+  puts "The flow_rate has been reset to #{flow_rate}."
+elsif flow_rate < 50
+  puts "Warning! flow_rate is below 50! It is #{flow_rate}"
+  flow_rate = 50
+  puts "The flow_rate has been reset to #{flow_rate}."
+else
+  puts "The flow_rate is #{flow_rate} (Thank goodness!)"
+end
